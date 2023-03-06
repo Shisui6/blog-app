e635ccbf210885b92b920c974abce354aea46559
Frehiywot Nega <fr.nega@gmail.com>
2023-03-06 16:16:43 +0300

    frix to n+1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where(user_id: params[:user_id])
+    @posts = Post.where(user_id: params[:user_id]).includes(:comments)
     @user = User.find(params[:user_id])
   end
 
