5b871f25320b2bd034c213695206c506d5723314
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-03-07 11:00:47 +0100

    Add integration specs

--- spec/controller/posts_controller_spec.rb
+++ spec/controller/posts_controller_spec.rb
@@ -1,27 +1,42 @@
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-    before(:example) { get '/users/1/posts' }
+    before(:example) do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVv', bio: 'Teacher', posts_counter: 0)
+
+      get user_posts_path user_id: user.id
+    end
+
+    after(:example) do 
+      Post.destroy_all
+      User.destroy_all 
+    end
+
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
       expect(response).to render_template('index')
     end
-    it 'contains the correct placeholder text' do
-      expect(response.body).to include('Here is a list of all posts for a user')
-    end
   end
 
   describe 'GET #show' do
-    before(:example) { get '/users/1/posts/1' }
+    before(:example) do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVv', bio: 'Teacher', posts_counter: 0)
+      post = Post.create(user:, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+
+      get user_post_path user_id: user.id, id: post.id
+    end
+
+    after(:example) do 
+      Post.destroy_all
+      User.destroy_all 
+    end
+
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
-    it "renders 'index' template" do
+    it "renders 'show' template" do
       expect(response).to render_template('show')
-    end
-    it 'contains the correct placeholder text' do
-      expect(response.body).to include('Here is the info for a particular post')
     end
   end
 end
--- spec/controller/users_controller_spec.rb
+++ spec/controller/users_controller_spec.rb
@@ -7,21 +7,24 @@ RSpec.describe 'Users', type: :request do
     it "renders 'index' template" do
       expect(response).to render_template('index')
     end
-    it 'contains the correct placeholder text' do
-      expect(response.body).to include('Here is a list of all users')
-    end
   end
 
   describe 'GET #show' do
-    before(:example) { get '/users/1' }
+    before(:example) do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVv', bio: 'Teacher', posts_counter: 0)
+      get user_path id: user.id
+    end
+
+    after(:example) do
+      Post.destroy_all
+      User.destroy_all
+    end
+
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
-    it "renders 'index' template" do
+    it "renders 'show' template" do
       expect(response).to render_template('show')
-    end
-    it 'contains the correct placeholder text' do
-      expect(response.body).to include('Here is the info for a particular user')
     end
   end
 end
